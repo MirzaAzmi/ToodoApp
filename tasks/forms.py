97820f2f02c5a0ddd0ffa31689830558a862160a
Mirza Azmi <azmimirza801@gmail.com>
2022-09-10 20:18:28 +0530

    'TODOLIST_app'

---
+++ tasks/forms.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from django import forms
+from django.forms import ModelForm
+from .models import Task
+
+class Taskform(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
